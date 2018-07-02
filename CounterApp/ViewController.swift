ca68723c390f00c76a7eb5a6fc74abd8e008614c
Juan Carlos <juancarlos@MacBook-Air-de-Mac.local>
2018-07-02 15:36:49 -0500

    Initial Commit

---
+++ CounterApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CounterApp
+//
+//  Created by Juan Carlos on 2/07/18.
+//  Copyright © 2018 Juan Carlos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
